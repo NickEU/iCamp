2abbdc27192a28a30f4439b7ac127425bb774984
Nick <neverate@gmail.com>
2020-06-22 11:57:14 +0300

    CH2-SubArrSumProblems: +indices upd kadane

--- icamp/aArraysAndStrings/ic05SubarraySums/maxSumKadane/Kadane.java
+++ icamp/aArraysAndStrings/ic05SubarraySums/maxSumKadane/Kadane.java
@@ -6,20 +6,44 @@ public class Kadane {
   public static void main(String[] args) {
     Kadane s = new Kadane();
     int[] maxSubArr = s.getSubArrWithMaxSum(new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4});
+    System.out.println(s.getMaxSum(new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4}));
     System.out.println(Arrays.toString(maxSubArr));
     System.out.println(Arrays.toString(s.getSubArrWithMaxSum(new int[] {-2, -10})));
-    System.out.println(Arrays.toString(s.getSubArrWithMaxSum(new int[] {2, -10, -5, 76})));
+    System.out.println(Arrays.toString(s.getSubArrWithMaxSum(new int[] {2, -10, -5, 76, 2})));
     System.out.println(Arrays.toString(s.getSubArrWithMaxSum(new int[] {50})));
-    System.out.println(Arrays.toString(s.getSubArrWithMaxSum(new int[] {-2, -10, -17})));
+    System.out.println(Arrays.toString(s.getSubArrWithMaxSum(new int[] {-22, 0, -17})));
+    System.out.println(Arrays.toString(s.getSubArrWithMaxSum(new int[] {1, 2, -1, 2, -3, 2, -5})));
+  }
+
+  private Integer getMaxSum(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      System.out.println("Error! Empty/null array given as parameter.");
+      return null;
+    }
+
+    int maxSum = nums[0];
+    int currSum = maxSum;
+
+    for (int i = 1; i < nums.length; i++) {
+      currSum = Math.max(currSum + nums[i], nums[i]);
+      maxSum = Math.max(currSum, maxSum);
+    }
+
+    return maxSum;
   }
 
   private int[] getSubArrWithMaxSum(int[] nums) {
-    int maxSum = Integer.MIN_VALUE;
-    int currSum = maxSum;
-    int currSize = 0;
-    SubArray result = null;
+    if (nums == null || nums.length == 0) {
+      System.out.println("Error! Empty/null array given as parameter.");
+      return null;
+    }
 
-    for (int i = 0; i < nums.length; i++) {
+    int maxSum = nums[0];
+    int currSum = maxSum;
+    int currSize = 1;
+    SubArray result = new SubArray(0, 1);
+
+    for (int i = 1; i < nums.length; i++) {
       int currEl = nums[i];
 
       if (currSum < 0) {
@@ -36,10 +60,6 @@ public class Kadane {
       }
     }
 
-    if (result == null) {
-      System.out.println("Error!");
-      return null;
-    }
     return result.buildNewSubArr(nums);
   }
 
