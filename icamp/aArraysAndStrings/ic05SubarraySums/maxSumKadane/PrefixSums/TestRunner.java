ef1c8c424b82b16e3e228ac899bbdc6a3b627d51
Nick <neverate@gmail.com>
2020-06-23 11:48:07 +0300

    CH2-SubArrPrefixSums: upd sums to zero testRunner

--- icamp/aArraysAndStrings/ic05SubarraySums/maxSumKadane/PrefixSums/TestRunner.java
+++ icamp/aArraysAndStrings/ic05SubarraySums/maxSumKadane/PrefixSums/TestRunner.java
@@ -2,19 +2,19 @@ package icamp.aArraysAndStrings.ic05SubarraySums.maxSumKadane.PrefixSums;
 
 public class TestRunner {
   public static void main(String[] args) {
-    runTestSuite(new FindSubArrThatSumsToZero(), LoggingMode.VERBOSE);
+    runTestSuite(new FindSubArrThatSumsToZero(), 0, LoggingMode.VERBOSE);
   }
 
-  private static void runTestSuite(ITestable o, LoggingMode logMode) {
+  private static void runTestSuite(ITestable o, int targetSum, LoggingMode logMode) {
     System.out.printf("Testing %s :%n", o.getClass().getSimpleName());
     System.out.println("***************");
-    o.getResult(new int[]{2, 4, -2, 1, -3, 5, -3}, 0, logMode);
-    o.getResult(new int[]{1, 5, -7, 0}, 0, logMode);
-    o.getResult(new int[]{2, 4, 6}, 0, logMode);
-    o.getResult(new int[]{2, 4, 6, -6, -4, -2}, 0, logMode);
-    o.getResult(new int[]{2, 4, 6, -2, -2, -2}, 0, logMode);
-    o.getResult(new int[]{-2}, 0, logMode);
-    o.getResult(new int[]{0}, 0, logMode);
+    o.getResult(new int[]{2, 4, -2, 1, -3, 5, -3}, targetSum, logMode);
+    o.getResult(new int[]{1, 5, -7, 0}, targetSum, logMode);
+    o.getResult(new int[]{2, 4, 6}, targetSum, logMode);
+    o.getResult(new int[]{2, 4, 6, -6, -4, -2}, targetSum, logMode);
+    o.getResult(new int[]{2, 4, 6, -2, -2, -2}, targetSum, logMode);
+    o.getResult(new int[]{-2}, targetSum, logMode);
+    o.getResult(new int[]{0}, targetSum, logMode);
     System.out.println("***************");
   }
 }
