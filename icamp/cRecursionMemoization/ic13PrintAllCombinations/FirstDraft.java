c453cb665198d0ef72b220ad5e654ce2c3c6fda4
Nick <neverate@gmail.com>
2020-06-26 16:54:40 +0300

    CH6-Recursion: +print all combinations bad version

---
+++ icamp/cRecursionMemoization/ic13PrintAllCombinations/FirstDraft.java
@@ -0,0 +1,20 @@
+package icamp.cRecursionMemoization.ic13PrintAllCombinations;
+
+public class FirstDraft {
+  static int[] input = {1, 2, 3, 4, 5};
+  static int limit = 3;
+  public static void main(String[] args) {
+    printCombinations(new StringBuilder(),0);
+  }
+
+  private static void printCombinations(StringBuilder prefix, int idx) {
+    if (prefix.length() == limit) {
+      System.out.println(prefix);
+      return;
+    }
+    for (int i = idx; i < input.length; i++) {
+      printCombinations(prefix.append(input[i]), i + 1);
+      prefix.deleteCharAt(prefix.length() - 1);
+    }
+  }
+}
