2a7963c7f0da00adc0b2e9ba08fa9e5736e96dc2
Nick <neverate@gmail.com>
2020-07-03 10:30:48 +0300

    CH6-Recursion: print all subsets improved

--- icamp/cRecursionMemoization/ic15PrintAllSubsets/Solution.java
+++ icamp/cRecursionMemoization/ic15PrintAllSubsets/Solution.java
@@ -13,36 +13,37 @@ package icamp.cRecursionMemoization.ic15PrintAllSubsets;
 // [2, 3]
 // [1, 2, 3]
 
-import java.util.Arrays;
-
 public class Solution {
   public static void main(String[] args) {
     int[] input = {1, 2, 3, 4, 5};
-    printAllSubsets(input);
+    printSubsetsHelper(input);
   }
 
-  private static void printAllSubsets(int[] input) {
-    for (int i = 0; i <= input.length; i++) {
-      System.out.println("ALl subsets of size " + i + ":");
-      printAllCombinations(input, new int[i], 0, 0);
-      System.out.println("\n");
-    }
+  private static void printSubsetsHelper(int[] input) {
+      printAllSubsets(input, new int[input.length], 0, 0);
   }
 
-  private static void printAllCombinations(
+  private static void printAllSubsets(
       int[] nums, int[] buffer, int numPos, int bufPos) {
-    if (bufPos == buffer.length) {
-      System.out.print(Arrays.toString(buffer) + " ");
-      return;
-    }
-
-    if (numPos == nums.length) {
+    printArray(buffer, bufPos);
+    if (bufPos == buffer.length || numPos == nums.length) {
       return;
     }
 
     for (int i = numPos; i < nums.length; i++) {
       buffer[bufPos] = nums[i];
-      printAllCombinations(nums, buffer, i + 1, bufPos + 1);
+      printAllSubsets(nums, buffer, i + 1, bufPos + 1);
     }
+  }
+
+  private static void printArray(int[] arr, int upToIdx) {
+    StringBuilder result = new StringBuilder("[");
+    for (int i = 0; i < upToIdx; i++) {
+      result.append(arr[i]);
+      if (i < upToIdx - 1) {
+        result.append(", ");
+      }
+    }
+    System.out.println(result.append("]"));
   }
 }
